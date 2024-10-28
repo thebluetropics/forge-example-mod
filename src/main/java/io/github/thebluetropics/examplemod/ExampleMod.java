28968611eb8c06ee7f5acd3b0c04d775e7aab6e1
thebluetropics <thebluetropics@gmail.com>
2024-10-28 11:47:57 +0700

    initial commit

---
+++ src/main/java/io/github/thebluetropics/examplemod/ExampleMod.java
@@ -0,0 +1,23 @@
+package io.github.thebluetropics.examplemod;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Mod(ExampleMod.ID)
+public class ExampleMod {
+  public static final String ID = "examplemod";
+  public static final Logger LOGGER = LoggerFactory.getLogger(ID);
+
+  public ExampleMod(FMLJavaModLoadingContext context) {
+    IEventBus modEventBus = context.getModEventBus();
+    modEventBus.addListener(this::commonSetup);
+  }
+
+  private void commonSetup(FMLCommonSetupEvent event) {
+    ExampleMod.LOGGER.info("Hello, World! (Common Setup)");
+  }
+}
---
+++ src/main/java/io/github/thebluetropics/examplemod/client/ExampleModClient.java
@@ -0,0 +1,15 @@
+package io.github.thebluetropics.examplemod.client;
+
+import io.github.thebluetropics.examplemod.ExampleMod;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(modid = ExampleMod.ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ExampleModClient {
+  @SubscribeEvent
+  public static void clientSetup(FMLClientSetupEvent event) {
+    ExampleMod.LOGGER.info("Hello, World!");
+  }
+}
